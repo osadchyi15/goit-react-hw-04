934ed65f752b10800416eec82c8844010a18f190
Yevgen <osadchyi15@gmail.com>
2024-08-15 23:43:01 +0200

    react-modal

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -48,30 +48,3 @@ const ImageModal = ({ modalUrl, modalAlt, isModalOpen, onCloseModal }) => {
 };
 
 export default ImageModal;
-
-// const ImageModal = ({ modalUrl, modalAlt, onCloseModal }) => {
-//
-
-//   return (
-//     <div onClick={handleBackDropClick} className={css.backdrop}>
-//       <div className={css.modal}>
-//         <button
-//           type="button"
-//           className={css.closeModalBtn}
-//           onClick={onCloseModal}
-//         >
-//           &times;
-//         </button>
-//         <img
-//           src={modalUrl}
-//           alt={modalAlt}
-//           className={css.imageModal}
-//           width="100%"
-//           height="100%"
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-//
-// export default ImageModal;
